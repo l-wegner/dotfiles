4f8d1438987e33456f160df61ec21b643e7921cb
Lars Wegner <lars.wegner.devs@gmail.com>
2024-08-05 08:21:09 +0200

    Add surround plugin to nvim

--- home/.config/nvchad/lua/plugins/init.lua
+++ home/.config/nvchad/lua/plugins/init.lua
@@ -55,5 +55,16 @@ return {
     keys = {
       { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
     }
+  },
+
+{
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
 }
 }
