f320147bf791fce53c4aeee1016e5d49f2c8f07d
Lars Wegner <lars.wegner.devs@gmail.com>
2024-09-17 08:12:25 +0200

    Add multiple languages to treesitter

--- home/.config/nvchad/lua/plugins/init.lua
+++ home/.config/nvchad/lua/plugins/init.lua
@@ -17,26 +17,62 @@ return {
   },
   --
   {
-  	"williamboman/mason.nvim",
-  	opts = {
-  		ensure_installed = {
-  			"lua-language-server", "stylua",
-  			"html-lsp", "css-lsp" , "prettier",
-        "haskell-language-server", "typescript-language-server",
-        "rust-analyzer"
-  		},
-  	},
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "haskell-language-server",
+        "typescript-language-server",
+        "rust-analyzer",
+      },
+    },
   },
   --
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "rust",
+        "yaml",
+        "zig",
+        "haskell",
+        "go",
+        "gomod",
+        "gosum",
+        "gowork",
+        "helm",
+        "jq",
+        "json",
+        "markdown",
+        "mermaid",
+        "make",
+        "proto",
+        "python",
+        "sql",
+        "ssh_config",
+        "toml",
+        "typescript",
+        "yaml",
+        "xml",
+        "arduino",
+        "awk",
+        "bash",
+        "dockerfile",
+        "git_config",
+        "git_rebase",
+        "gitcommit",
+      },
+    },
+  },
   {
     "kdheepak/lazygit.nvim",
     cmd = {
@@ -53,18 +89,18 @@ return {
     -- setting the keybinding for LazyGit with 'keys' is recommended in
     -- order to load the plugin when the command is run for the first time
     keys = {
-      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-    }
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
   },
 
-{
+  {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
     config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
-    end
-}
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
 }
