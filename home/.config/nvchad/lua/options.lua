eb63438eb1201d90e02b359cb7f2d4a7a9c2211e
Lars Wegner <lars.wegner.devs@gmail.com>
2024-09-17 08:14:12 +0200

    Modify multiple nvchad things
    - enable folding with treesitter
    - enable highlighting on yank
    - disable mapping ; to :
    - enable offset scrolling

--- home/.config/nvchad/lua/options.lua
+++ home/.config/nvchad/lua/options.lua
@@ -2,5 +2,23 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
+local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+ 
+o.foldmethod = "expr"
+o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+o.scrolloff = 10
+o.cursorline = true
+
+
+local api = vim.api
+
+
+api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking text",
+  group = api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
