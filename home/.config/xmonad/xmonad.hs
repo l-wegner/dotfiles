b9bf75aabd0915edf3af797aa46f8d2911850bc2
Lars Wegner <lars.wegner.devs@gmail.com>
2025-07-09 07:10:52 +0200

    Fix scratchpad terminal
    Signed-off-by: Lars Wegner <lars.wegner.devs@gmail.com>

--- home/.config/xmonad/xmonad.hs
+++ home/.config/xmonad/xmonad.hs
@@ -314,7 +314,7 @@ myScratchPads =
     NS "web" spawnFirefox findFirefox manageFirefox
   ]
   where
-    spawnTerm = myTerminal ++ " -t scratchpadTerm -e tmux attach-session -t  scratch || tmux new -s scratch"
+    spawnTerm = myTerminal ++ " -t scratchpadTerm -e zsh -c 'tmux attach-session -t  scratch || tmux new -s scratch'"
     findTerm = title =? "scratchpadTerm"
     manageTerm = customFloating $ W.RationalRect l t w h
       where
